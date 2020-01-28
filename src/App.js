23dd1d910d8408056264fa92970de532a872343d
kalle <carl.hammar@gmail.com>
2020-01-28 09:08:12 +0100

    updating our app

--- src/App.js
+++ src/App.js
@@ -4,34 +4,12 @@ import React from 'react';
 import './App.scss';
 
 import HomePage from './pages/homepage/homepage.component';
+
+
+
+
 import { Switch, Route, Link } from 'react-router-dom';
-
-const HatsPage = (props)=>{
-  
-  
-  return(
-    <div>
-      <button onClick={() => props.history.push( '/topics/:topicId')}>click me</button>
-      
-      <h1>HatsPage</h1>
-     
-      <p> Fan funka nu</p>
-    </div>
-
-  );
-};
-
-const TopicList=(props)=>{
-  console.log(props)
-  return(
-    <div>
-<h1>hello mannen {props.match.params.topicId}</h1>
-      <p> korv </p>
-    </div>
-  );
-};
-
-
+import ShopPage from './pages/shop/shop.component.jsx';
 
 
 
@@ -41,8 +19,8 @@ function App() {
     <div>
      <Switch>
        <Route exact path='/' component={HomePage} />
-       <Route exact path='/hats' component={HatsPage}/>
-       <Route path='/topics/:topicId' component={TopicList}/>
+       <Route  path='/shop' component={ShopPage}/>
+      
        
      </Switch>
     
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { withRouter} from 'react-router-dom';
+import { withRouter} from 'react-router-dom'; // higher-Order Components
 
 import '../menu-item/menu-item.styles.scss';
 
